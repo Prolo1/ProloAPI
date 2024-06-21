0e68c471ddc80601fe00b60aaa14871d7296b8b5
Prolo <prolofresh@gmail.com>
2024-06-21 10:41:12 -0400

    Skeleton code

---
+++ Common/Extentions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProloAPI
+{
+
+	namespace Extentions
+	{
+		public static class Util
+		{
+
+			public static void Init() { }
+
+			public static class General
+			{
+			}
+			public static class Game
+			{
+			}
+			public static class GUI
+			{
+			}
+		}
+	}
+}
