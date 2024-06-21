8281e02040e48ea670c8cf98c41c90457929baba
Prolo <prolofresh@gmail.com>
2024-06-21 11:27:30 -0400

    Reorganized

---
+++ Extentions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProloAPI
+{
+
+	namespace Extentions
+	{
+		public static class Util
+		{
+			internal static ManualLogSource Logger { get; private set; }
+
+			static Texture2D _greyTex = null;
+			public static Texture2D greyTex
+			{
+				get
+				{
+					if(_greyTex != null) return _greyTex;
+
+					_greyTex = new Texture2D(1, 1);
+					var pixels = _greyTex.GetPixels();
+					for(int a = 0; a < pixels.Length; ++a)
+						pixels[a] = Color.black;
+					_greyTex.SetPixels(pixels);
+					_greyTex.Apply();
+
+					return _greyTex;
+				}
+			}
+			public static void Init(ManualLogSource logger) { Logger = logger; }
+
+			public static class General
+			{
+			}
+			public static class Game
+			{
+			}
+			public static class GUI
+			{
+			}
+		}
+	}
+}
